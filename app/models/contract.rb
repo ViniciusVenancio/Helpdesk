22dc8e3bf32ccdcfe89ce2e8e4d4de4c7c03adbb
ViniciusVenancio <vinicius.venancio@live.com>
2015-04-28 13:42:17 -0300

    fixing css

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -13,10 +13,19 @@ class Contract < ActiveRecord::Base
 	validates :sla, presence:true
 	validates :status, presence:true
 	validates_presence_of :slug
+	validate :equal_date, :wrong_monthly_payment
 
 	friendly_id :contract_slug_enterpise, use: [:slugged, :history]
 
 	def contract_slug_enterpise
 		"#{self.enterprise.corporate_name}"
+	end
+
+	def equal_date
+		errors.add("due_date", "data de vencimento igual a data de início!") unless self.start_date != self.due_date
+	end
+
+	def wrong_monthly_payment
+		errors.add("monthly_payment", "Valor mensal inválido") unless self.monthly_payment > 0 && self.monthly_payment.is_a?(Numeric)
 	end
 end
